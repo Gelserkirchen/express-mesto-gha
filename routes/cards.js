264be45ade0176a7fc24d0feeca82cef8fafa17e
Andrei <gelserkirchen@mail.ru>
2022-08-02 09:49:45 +0300

    add joi check

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,35 @@
 const cardsRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const express = require('express');
 const {
   createCard, getCards, deleteCardById, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-cardsRouter.post('/', createCard);
-cardsRouter.get('/', getCards);
-cardsRouter.delete('/:cardId', deleteCardById);
+cardsRouter.post('/', express.json(), celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().uri({ scheme: ['http', 'https'] }),
+  }),
+}), createCard);
 
-cardsRouter.put('/:cardId/likes', likeCard);
-cardsRouter.delete('/:cardId/likes', dislikeCard);
+cardsRouter.get('/', getCards);
+
+cardsRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), deleteCardById);
+
+cardsRouter.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), likeCard);
+
+cardsRouter.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), dislikeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,30 @@
 const userRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const express = require('express');
+
 const {
   getUsers, getUsersById, updateAvatar, updateProfile, getMe,
 } = require('../controllers/users');
 
 userRouter.get('/', getUsers);
 userRouter.get('/me', getMe);
-userRouter.get('/:userId', getUsersById);
+userRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUsersById);
 
-userRouter.patch('/me', updateProfile);
-userRouter.patch('/me/avatar', updateAvatar);
+userRouter.patch('/me', express.json(), celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+}), updateProfile);
+
+userRouter.patch('/me/avatar', express.json(), celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().uri({ scheme: ['http', 'https'] }),
+  }),
+}), updateAvatar);
 
 module.exports = userRouter;
