0b9f3255dea8d576f4a486e89c85862bdce35b67
Andrei <gelserkirchen@mail.ru>
2022-08-05 13:42:10 +0300

    change regex

--- utils/urlRegex.js
+++ utils/urlRegex.js
@@ -1,3 +1,4 @@
-const urlRegex = /(https?:\/\/)?([\da-z\.-]+)\.([a-z]{2,6})([\/\w\.-]*)*\/?/g;
+// eslint-disable-next-line no-useless-escape
+const urlRegex = /(https?:\/\/)?([\da-z\.-]+)\.([a-z]{2,6})([\/\w\.-]*)*\/?/;
 
 module.exports = urlRegex;
